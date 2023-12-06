06799c265bb71b4402161ad3f9fafbe5f875e7a5
man-coding <chch2857@gmail.com>
2023-12-06 13:44:49 +0900

    231206 upd

--- src/string/quiz01.java
+++ src/string/quiz01.java
@@ -3,10 +3,18 @@ package string;
 public class quiz01 {
 
 	public static void main(String[] args) {
-		
+
 		String str = "a:b:c:d";
-		
+
 		System.out.println(str.replace(":", "#"));
+
+		
+		
+		String str1 = "안녕하세요 저는 둘리입니다";
+
+		System.out.println(str1.indexOf("둘"));
+
+		System.out.println(str1.substring(9, 11));
 	}
 
 }
--- src/string/quiz02.java
+++ src/string/quiz02.java
@@ -3,14 +3,15 @@ package string;
 public class quiz02 {
 
 	public static void main(String[] args) {
-		
-		String str = "안녕하세요 저는 둘리입니다";
-		
-		System.out.println(str.indexOf("둘"));
-		
-		System.out.println(str.substring(9,11));
-		
-				
+
+		String personNum = "881120-1068234";
+		System.out.println(personNum.indexOf("-") + 1);
+		if (personNum.charAt(7) == '1' || personNum.charAt(7) == '3') {
+			System.out.println("성별은 남자입니다.");
+		} else {
+			System.out.println("성별은 여자입니다.");
+		}
+
 	}
 
 }
